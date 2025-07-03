ba13882e203be42d51188601b593f593fe036afe
FrixxerNynd <julifer2435@gmail.com>
2025-07-03 12:30:31 -0600

    Actualizacion Data service

--- Back/data/src/Routes/dataRoutes.js
+++ Back/data/src/Routes/dataRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import { crearRegistro, obtenerRegistros, obtenerPorId, actualizarRegistro, eliminarRegistrosA } from '../Controller/data.controller.js';
-
+import { verifyToken } from '../Middleware/auth.middleware.js';
 
 const router = express.Router();
-router.post('/', crearRegistro);
-router.get('/', obtenerRegistros);
-router.get('search/:id', obtenerPorId);
-router.put('update/:id', actualizarRegistro);
-router.delete('delete', eliminarRegistrosA);
+
+// Rutas protegidas con token
+router.post('/', verifyToken, crearRegistro);
+router.get('/', verifyToken, obtenerRegistros);
+router.get('/search/:id', verifyToken, obtenerPorId);
+router.put('/update/:id', verifyToken, actualizarRegistro);
+router.delete('/delete', verifyToken, eliminarRegistrosA);
 
 export default router;
